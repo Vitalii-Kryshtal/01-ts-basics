4c39afb5d0279d217023aa0433558ea2b9918e83
Vitalii Kryshtal <krishtal.vtalk@ukr.net>
2025-05-08 19:54:20 +0300

    ts-hw-fix

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,19 +1,18 @@
 import axios from "axios";
 
 interface Post {
-    userId: number,
-    id: number,
-    title: string,
-    body: string,
+    id: number;
+    title: string;
+    body: string;
 }
 
 async function fetchPosts(): Promise<Post[]> {
   const response = await axios.get<Post[]>(
     'https://jsonplaceholder.typicode.com/posts'
   );
-      return response.data;
+  return response.data;
 }
 
 fetchPosts().then((posts) => {
-    console.log(posts[0].title);    
+  console.log(posts[0].title);
 });
